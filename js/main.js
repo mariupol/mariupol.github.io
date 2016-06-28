72b58607f89c18df9e819728fe11027a39e528e4
ekolo <e_kolo@mail.ru>
2016-06-28 17:48:00 +0400

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
 $(window).load(function() {
-
     $(".loader_inner").fadeOut();
     $(".loader").delay(400).fadeOut("slow");
-
-});
-
-// Scroll to top button
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-        $('.scrolltotop').fadeIn();
-    } else {
-        $('.scrolltotop').fadeOut();
-    }
-});
-// Click event to scroll to top
-$('.scrolltotop').click(function(){
-    $('html, body').animate({scrollTop : 0}, 1500, 'easeInOutExpo');
-    return false;
-});
-
-// Close mobile menu when click menu link (Bootstrap default menu)
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-        $(this).collapse('hide');
-    }
 });
 
 //PLUGIN SCRIPT FOR POPUP IMAGE
